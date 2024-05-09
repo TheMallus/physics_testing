30cace8a781a5b87abfa967aaf1498670d4eabca
TheMallus <162246706+TheMallus@users.noreply.github.com>
2024-05-09 17:41:11 -0500

    Update components.rs

--- src/components.rs
+++ src/components.rs
@@ -1,1 +1,29 @@
+use bevy::ecs::component::Component;
 
+
+#[derive(Component)]
+pub struct Center {
+    pub min_size: f32,
+    pub max_size: f32,
+    pub scale_factor: f32,
+}
+
+#[derive(Component)]
+pub struct Despawn;
+
+/// Health points.
+#[derive(Component)]
+pub struct Health {
+    pub hp: i32,
+    pub max_hp: i32,
+
+}
+
+impl Default for Health {
+    fn default() -> Self {
+        Self {
+            hp: 100,
+            max_hp: 100,
+        }
+    }
+}
