785080f4460b2072f17d472557ca3d1addbf096a
TheMallus <162246706+TheMallus@users.noreply.github.com>
2024-05-09 17:38:40 -0500

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,143 +1,29 @@
-use bevy::{prelude::*, render::mesh::shape::Cube};
-use bevy_rapier3d::{prelude::*, rapier::{control::EffectiveCharacterMovement, dynamics::RigidBodyHandle}};
+use::bevy::prelude::*;
+use bevy_rapier3d::prelude::*;
 use bevy_egui::{egui, EguiContexts, EguiPlugin};
-use std::time::Duration;
+use rand::*;
 
 fn main() {
-    App::new() 
-        .add_plugins(DefaultPlugins)
-        .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
-        .add_plugins(RapierDebugRenderPlugin::default())
-        .add_systems(Startup, setup)
-        .add_systems(Startup, setup_graphics)
-        .add_systems(Update, (update_system, read_result_system))
-        // .add_systems(Update, cast_ray)
-        .run();
-    
+    App::new()
+    .add_plugins(DefaultPlugins)
+    .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
+    .add_plugins(RapierDebugRenderPlugin::default())
+    .add_plugins(EguiPlugin)
+    .add_systems(Startup, setscene)
+    .add_systems(Update, ui)
+    .run();
 }
 
-fn setup(mut commands: Commands, mut materials: ResMut<Assets<StandardMaterial>>, mut meshes: ResMut<Assets<Mesh>>) {
-    let x_size = 1.0;
-    commands.spawn((
-        RigidBody::KinematicPositionBased,
-        PbrBundle {
-            transform: Transform::from_xyz(1.1, 0.0, -0.8),
-            mesh: meshes.add(Mesh::from(shape::Cube::new(2.0))),
-            material: materials.add(Color::NAVY).into(),
-            ..default()
-        },
-        Collider::cuboid(x_size, x_size, x_size),
-        ColliderMassProperties::Density(2.0),
-        KinematicCharacterController {
-            up: Vec3::Y,
-            ..default()
-        },
-        GravityScale(0.5),
-        Ccd::enabled(),
-        Sleeping::disabled(),
-        Center {
-            max_size: 2.0,
-            min_size: 1.0,
-            scale_factor: 8.0,
-        },
-        Despawn,
-        Health {
-            health: 250.0,
-        },
-    ));
-    commands.spawn((
-        RigidBody::Fixed,
-        PbrBundle {
-            transform: Transform::from_xyz(0.0, 0.0, 0.0),
-            mesh: meshes.add(Mesh::from(shape::Cube::new(100.0))),
-            material: materials.add(Color::WHITE).into(),
-            ..default()
-        },
-        Collider::cuboid(100.0, 0.1, 100.0),
-        ColliderMassProperties::Density(10.0),
-        Ccd::disabled(),
+fn setscene(mut commands: Commands) { // ADD TO parameter if uncommenting / ( / (mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>> / ) /
+    // commands.spawn(PbrBundle {
+    //     mesh: meshes.add(Circle::new(15.0)),
+    //     material: materials.add(Color::WHITE),
+    //     transform: Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
+    //     ..default()
+    // });
+    commands.spawn(Collider::cuboid(200.0, 0.1, 200.0))
+            .insert(TransformBundle::from(Transform::from_xyz(0.0, -2.0, 0.0)));
 
-    ));
-
-    let num = 5;
-    let rad = 1.0;
-
-    let shift = rad * 2.0 + rad;
-    let centerx = shift + 2.0 * (num / 2) as f32;
-    let centery = shift / 2.0;
-    let centerz = shift + 2.0 * (num / 2) as f32;
-
-    let mut offset = -(num as f32) * (rad * 2.0 + rad) * 0.5;
-    let mut color = 0;
-    let colors = [
-        Color::hsl(220.0, 1.0, 0.3),
-        Color::hsl(180.0, 1.0, 0.3),
-        Color::hsl(260.0, 1.0, 0.7),
-    ];
-
-    for j in 0usize..10 {
-        for i in 0..num {
-            for k in 0usize..num {
-                let x = i as f32 * shift - centerx + offset + -10.0;
-                let y = j as f32 * shift + centery + 3.0;
-                let z = k as f32 * shift - centerz + offset + 2.0;
-                color += 1;
-
-                commands
-                    .spawn(TransformBundle::from(Transform::from_rotation(
-                        Quat::from_rotation_x(0.2),
-                    )))
-                    .with_children(|child| {
-                        child.spawn((
-                            PbrBundle {
-                                transform: Transform::from_xyz(x,y,z),
-                                mesh: meshes.add(Mesh::from(shape::Cube::new(2.0))),
-                                material: materials.add(Color::WHITE).into(),
-                                ..default()
-                            },
-                            RigidBody::Dynamic,
-                            Collider::cuboid(rad, rad, rad),
-                            ColliderDebugColor(colors[color % 3]),
-                            Ccd::enabled(),
-                            Health {
-                                health: 100.0,
-                            },
-                        ));
-                    });
-            }
-        }
-
-    //     offset -= 0.05 * rad * (num as f32 - 1.0);
-     }
-
-    // Create a text entity to display the health
-    // commands
-    //   .spawn(TextBundle {
-    //         text: Text {
-    //             sections: vec![TextSection {
-    //                 value: "Health: 100".to_string(),
-    //                 style: TextStyle {
-    //                     color: Color::rgb(1.0, 1.0, 1.0),
-    //                 },
-    //             }],
-    //             alignment: TextAlignment {
-    //                 horizontal: HorizontalAlign::Center,
-    //                 vertical: VerticalAlign::Center,
-    //             },
-    //         },
-    //         transform: Transform::from_translation(Vec3::new(0.0, 0.0, 10.0)),
-    //        ..Default::default()
-    //     })
-    //   .insert(HealthText);
-}
-// fn update_health_text(mut query: Query<(&Health, &mut Text)>, mut health_text_query: Query<&mut Text, With<HealthText>>) {
-//     for (health, mut text) in query.iter_mut() {
-//         for mut health_text in health_text_query.iter_mut() {
-//             health_text.sections[0].value = format!("Health: {}", health.0);
-//         }
-//     }
-// }
-fn setup_graphics(mut commands: Commands) {
     commands.spawn(PointLightBundle {
         point_light: PointLight {
             shadows_enabled: true,
@@ -147,123 +33,158 @@ fn setup_graphics(mut commands: Commands) {
         ..default()
     });
     // camera
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(44.0, 30.0, 40.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
-    });
-}
-fn update_system(mut controllers: Query<&mut KinematicCharacterController>, keys: Res<ButtonInput<KeyCode>>,
-    mut centers: Query<(&mut Transform, &Center)>, mut commands: Commands, query: Query<Entity, With<Despawn>>,
-    mut materials: ResMut<Assets<StandardMaterial>>, mut meshes: ResMut<Assets<Mesh>>) {
-    for mut controller in controllers.iter_mut() {
-            if keys.pressed(KeyCode::KeyD) {
-                controller.translation = Some(Vec3::new(0.1, 0.0, -0.1));
-            }
-            if keys.pressed(KeyCode::KeyA) {
-                controller.translation = Some(Vec3::new(-0.1, 0.0, 0.1));
-            }
-            if keys.pressed(KeyCode::KeyW) {
-                controller.translation = Some(Vec3::new(-0.1, 0.0, -0.1));
-            }
-            if keys.pressed(KeyCode::KeyS) {
-                controller.translation = Some(Vec3::new(0.1, 0.0, 0.1));
-            }
-            // if keys.pressed(KeyCode::Space) {
-            //     controller.translation = Some(Vec3::new(0.0, 1.0, 0.0));
-            // }
-            if keys.pressed(KeyCode::KeyH) {
-                commands.spawn((
-                    RigidBody::Dynamic,
-                    PbrBundle {
-                        transform: Transform::from_xyz(0.0,0.5,0.0),
-                        mesh: meshes.add(Mesh::from(Sphere::new(1.0))),
-                        material: materials.add(Color::RED).into(),
-                        ..default()
-                    },
-                    Collider::ball(1.0),
-                    Ccd::enabled(),
-                    Velocity {
-                        linvel: Vec3::new(1.0,-1.0,1.0),
-                        angvel: Vec3::new(0.0,0.0,0.0)
-                    },
-                    ColliderMassProperties::Density(2.0),
-                ));
-            }
-
-            if keys.pressed(KeyCode::KeyJ) {
-                for (mut transform, center) in &mut centers {
-                    transform.scale = Vec3::splat(center.scale_factor);
-                }
-            }
-
-    }
     
+
+    commands.spawn(RigidBody::Dynamic)
+            .insert(Collider::ball(0.5))
+            .insert(Restitution::coefficient(0.7))
+            .insert(TransformBundle::from(Transform::from_xyz(0.0, 4.0, 0.0)))
+            .insert(Ccd::enabled());
+    
+    commands.spawn(RigidBody::KinematicPositionBased)
+            .insert(TransformBundle::default())
+            .insert(Collider::cuboid(5.0, 10.0, 5.0))
+            .insert(KinematicCharacterController::default())
+            .insert(GravityScale(0.5))
+            .insert(ColliderMassProperties::Density(2.0));
+
+
+    commands.spawn(Camera3dBundle {
+                transform: Transform::from_xyz(-10.0, 25.0, 10.0).looking_at(Vec3::new(0.0, 10.0, 0.0),Vec3::Y),
+                ..Default::default()
+    });
+
+    bed(&mut commands, Vec3::new(20.0, 0.0, 0.0));
+
+    let block = commands.spawn((
+        TransformBundle::from(Transform::from_xyz(-5.0, 5.0, -5.0)),
+        RigidBody::Fixed,
+        Collider::cuboid(1.0, 1.0, 1.0),
+    )).id();
+    let x = Vec3::X;
+    let joint = RevoluteJointBuilder::new(x)
+    .local_anchor1(Vec3::new(0.0, 0.0,  1.0))
+    .local_anchor2(Vec3::new(0.0, 0.0, -3.0));
+
+    commands.entity(block).insert(ImpulseJoint::new(block, joint));
+
 }
+fn ui(mut contexts: EguiContexts) {
+    egui::Window::new("Stats").show(contexts.ctx_mut(), |ui| {
+        ui.label("Stat 1");
+    });
+} 
 
 
+fn bed(commands: &mut Commands, origin: Vec3) {
+    let rad = 0.4;
+    let shift = 2.0;
+    let mut curr_parent = commands.spawn((
+        TransformBundle::from(Transform::from_xyz(origin.x, origin.y, 0.0)),
+        RigidBody::Fixed,
+        Collider::cuboid(rad * 4.0, rad, rad),
+    ))
+    .id();
+    for i in 0..1 {
+        let z = origin.z + i as f32 * shift * 2.0 + shift;
+        let positions = [Vec3::new(origin.x, origin.y, z),
+                                    Vec3::new(origin.x + shift, origin.y, z),
+                                    Vec3::new(origin.x + shift, origin.y, z + shift),
+                                    Vec3::new(origin.x, origin.y, z + shift),];
+        
+        let mut handles = [curr_parent; 4];
+        for k in 0..4 {
+            handles[k] = commands.spawn((
+                TransformBundle::from(Transform::from_translation(positions[k])),
+                RigidBody::Dynamic,
+                Collider::cylinder(rad, rad),
+            )).id();
+        }
 
-fn read_result_system(controllers: Query<(Entity, &KinematicCharacterControllerOutput)>) {
-    for (entity, output) in controllers.iter() {
-        println!("Entity {:?} moved by {:?} and touches the ground: {:?}",
-                  entity, output.effective_translation, output.grounded);
+        let x = Vec3::X;
+        let z = Vec3::Z;
+        let revs = [
+            RevoluteJointBuilder::new(z).local_anchor2(Vec3::new(0.0, 0.0, -shift)),
+            RevoluteJointBuilder::new(x).local_anchor2(Vec3::new(-shift, 0.0, 0.0)),
+            RevoluteJointBuilder::new(z).local_anchor2(Vec3::new(0.0, 0.0, -shift)),
+            RevoluteJointBuilder::new(x).local_anchor2(Vec3::new(shift, 0.0, 0.0)),
+        ];
+
+        commands.entity(handles[0]).insert(ImpulseJoint::new(curr_parent, revs[0]));
+        commands.entity(handles[1]).insert(ImpulseJoint::new(curr_parent, revs[1]));
+        commands.entity(handles[2]).insert(ImpulseJoint::new(curr_parent, revs[2]));
+        commands.entity(handles[3]).insert(ImpulseJoint::new(curr_parent, revs[3]));
+        curr_parent = handles[3];
     }
 }
 
-fn cast_ray(rapier_context: Res<RapierContext>) {
-    let ray_pos = Vec3::new(1.0,2.0,1.0);
-    let ray_dir = Vec3::new(0.0, 0.01,0.0);
-    let max_toi = 10.0;
-    let solid = true;
-    let filter = QueryFilter::default();
-    if let Some((entity, toi)) = rapier_context.cast_ray(ray_pos, ray_dir, max_toi, solid, filter) {
-        let hit_point = ray_pos + ray_dir * toi;
-        println!("Entity {:?} hit at point {}", entity, hit_point);
-    }
-    if let Some((entity, intersection)) = rapier_context.cast_ray_and_get_normal(ray_pos, ray_dir, max_toi, solid, filter) {
-        let hit_point = intersection.point;
-        let hit_normal = intersection.normal;
-        println!("Entity {:?} hit at point {} with normal {}", entity, hit_point, hit_normal);
-    }
-    rapier_context.intersections_with_ray(ray_pos, ray_dir, max_toi, solid, filter,
-    |entity, intersection| {
-        let hit_point = intersection.point;
-        let hit_normal = intersection.normal;
-        println!("Entity {:?} hit at point {} with normal {}", entity, hit_point, hit_normal);
-        true
-    },
-    );
-}
 
-// fn update_health(mut query: Query<(&mut Health, &RigidBodyVelocity)>, quer: Query<Entity, With<Despawn>>, mut commands: Commands) {
-//     for (mut health, velocity) in &mut query {
-//         if velocity > 1.0 {
-//             health.current -= 10.0;
-//         }
-//         if health.current < 0.0 {
-//             for entity in &query {
-//                     println!("Ded");
-//                     commands.entity(entity).despawn();
-//                 }
+// fn bullet_fire(mut query: Query<(&mut Transform, &mut Laser, Entity)>, time: Res<Time>, mut commands: Commands) {
+//     for (mut transform, mut laser, entity) in query.iter_mut() {
+//         laser.velocity *= 1.0 + time.delta_seconds() * 6.0;
+//         transform.translation += laser.velocity * time.delta_seconds();
+//         if transform.translation.distance(Vec3::ZERO) > 50.0 {
+//             commands.entity(entity).despawn_recursive();
 //         }
 //     }
 // }
-
-
+// fn spawn_bullet(query: Query<(Entity, &AwaitingSpawnGun)>,
+//     children: Query<&Children>, names: Query<&Name>, mut commands: Commands) {
+//         for (entity, spawn_gun) in query.iter() {
+//             let mut spawned_guns = false;
+//             for child in Children.iter_descendants(entity) {
+//                 let Ok(name) = names.get(child) else {
+//                     continue;
+//                 };
+//                 if name.contains("laser") {
+//                     spawned_guns = true;
+//                     commands.entity(child).insert(Gun {
+//                         color: spawn_gun.color,
+//                         timer: Timer::from_seconds(
+//                             rand::thread_rng().gen_range(0.6..1.3),
+//                             TimerMode::Repeating,
+//                         ),
+//                     })
+//                 }
+//             }
+//         }
+//     }
 
 #[derive(Component)]
-struct Center {
-    min_size: f32,
-    max_size: f32,
-    scale_factor: f32,
+pub struct Laser {
+    pub velocity: Vec3,
 }
 
 #[derive(Component)]
-struct Despawn;
-
-#[derive(Component)]
-struct Health {
-    health: f32,
+pub struct Gun {
+    pub color: Color,
 }
 
 #[derive(Component)]
-struct HealthText;
+pub struct AwaitingSpawnGun {
+    pub color: Color,
+}
+
+
+pub fn cast_ray(mut commands: Commands, windows: Query<&Window, With<PrimaryWindow>>,
+        rapier_context: Res<RapierContext>, cameras: Query<(&Camera, &GlobalTransform)>,) {
+            let window = windows.single();
+            let Some(cursor_position) = window.cursor_position() else {
+                return;
+            };
+            for (camera, camera_transform) in &cameras {
+                let Some(ray) = camera.viewport_to_world(camera_transform, cursor_position) else {
+                    return;
+                };
+                let hit = rapier_context.cast_ray(ray.origin, ray.direction.into(),
+                 f32::MAX, true, QueryFilter::only_dynamic(),
+                );
+                if let Some((entity, _toi)) = hit {
+                    let color = Color::BLUE;
+                    commands.entity(entity).insert(ColliderDebugColor(color));
+                }
+            }
+        }
+
+#[derive(Component, Reflect)]
+pub struct PrimaryWindow;
